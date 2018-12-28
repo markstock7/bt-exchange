9811cd20beed82171405e61291f34fce3a6bedcb
markstock <markstock7@hotmail.com>
2018-12-28 12:34:25 +0800

    Add ParseException Handler

--- exchange-base/src/main/scala/com/blocktrending/exchange/base/WebsocketListenerImpl.scala
+++ exchange-base/src/main/scala/com/blocktrending/exchange/base/WebsocketListenerImpl.scala
@@ -1,13 +1,17 @@
 package com.blocktrending.exchange.base
 
-import com.blocktrending.util.http.{ApiExceptionCause, ApiExceptionMsg, ParseException, WebSocketConnectionClosing}
+import com.blocktrending.util.http.ParseException
 import io.circe._
-import okhttp3.{Response, WebSocket, WebSocketListener}
+import okhttp3.{WebSocket, WebSocketListener}
 
-class WebsocketListenerImpl[T: Decoder](var callback: Either[Exception, T] => Unit) extends WebSocketListener {
+trait BWebSocketListener extends WebSocketListener {
+	def onParseFailure(webSocket: WebSocket, error: ParseException): Unit = println(error.toString)
+}
+
+class WebsocketListenerImpl[T: Decoder](var callback: T => Unit) extends BWebSocketListener {
 	override def onMessage(webSocket: WebSocket, text: String): Unit =
 		parser.decode[T](text) match {
-			case Right(event) => callback(Right(event))
-			case Left(error)  => callback(Left(ParseException(s"Couldn't decode $text: $error")))
+			case Right(event) => callback(event)
+			case Left(error)  => onParseFailure(webSocket, ParseException(s"Couldn't decode $text: $error"))
 		}
 }
